a3abe9d49ee335613e44cbf9953b71c2414238e5
Nate <7nater.nelson@gmail.com>
2021-03-19 15:34:06 -0600

    commit tree 8155bb9acdfd96d20a81eed6d9dbbd514c33a012

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def show
-    @recipe = Recipe.find(params[:id])
+    set_recipe
   end
 
   def index
@@ -9,8 +9,20 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build()
+    @recipe.ingredients.build()
   end
 
   def create
+    @recipe = Recipe.create(recipe_params) 
+  end
+
+  private 
+  def set_recipe 
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 end
